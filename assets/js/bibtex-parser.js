c936e831ab754b527fb645e8279aac473ab3ad17
Orlando Romeo <54634741+omromeo@users.noreply.github.com>
2025-08-22 14:18:38 -0700

    Update bibtex-parser.js

--- assets/js/bibtex-parser.js
+++ assets/js/bibtex-parser.js
@@ -43,21 +43,6 @@ function toTitleCase(str) {
     }).join(' ');
 }
 
-// Load Google Scholar metrics from local JSON
-async function loadScholarMetrics() {
-  try {
-    const response = await fetch('data/google-scholar.json');
-    const data = await response.json();
-    document.getElementById('scholar-sidebar').innerHTML = `
-      <p><strong>H-index:</strong> ${data.hindex}</p>
-      <p><strong>Citations:</strong> ${data.citations}</p>
-    `;
-  } catch (err) {
-    console.error('Error loading scholar metrics:', err);
-    document.getElementById('scholar-sidebar').innerText = 'Unable to load metrics';
-  }
-}
-
 async function loadPublications() {
   const response = await fetch('data/publications.bib');
   if (!response.ok) {
